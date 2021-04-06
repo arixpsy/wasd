cfc1c0a112799ae37a0ba90bea63b927e59be81e
Arix <arixgg@gmail.com>
2021-04-06 22:02:51 +0800

    generate key sequence for arrow and letter

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,29 +1,31 @@
 import React, { useEffect } from 'react'
 import styled from 'styled-components'
+import useGame from './../../hooks/useGame'
 
 const Console = styled.div`
-
+  display: grid;
+  grid-template-rows: 1fr auto;
 `
-
 const ViewSplittor = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
 `
-
 const Game = () => {
-  
+  const { sequence, generateSequence } = useGame()
   useEffect(() => {
-    // generate game input
+    generateSequence()
     // add button listener
     // add timer on start
     // stop timer on complete
   }, [])
 
+  console.log(sequence)
+
   return (
     <Console>
       <ViewSplittor>
-        <div>Hello</div>
-        <div>Hello</div>
+        <div>{sequence.letter.get}</div>
+        <div>{sequence.arrow.get}</div>
       </ViewSplittor>
       <div>Progress</div>
     </Console>
---
+++ src/hooks/useGame.js
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import _ from 'lodash'
+
+const GENERATE_COUNT = 10
+const INPUT_TYPE = Object.freeze({
+  ARROW: 'arrow',
+  LETTER: 'letter'
+})
+const INPUTS = Object.freeze({
+  ARROW: ['up', 'down', 'left', 'right'],
+  LETTER: ['w', 'a', 's', 'd']
+})
+
+const useGame = () => {
+  const [letterSequence, setLetterSequence] = useState([])
+  const [arrowSequence, setArrowSequence] = useState([])
+  // const [progress, setProgress] = useState(0)
+
+  const generateAllSequence = () => {
+    let sequenceArrow = []
+    let sequenceLetter = []
+    for (let i = 0; i < GENERATE_COUNT; i++) {
+      sequenceArrow.push(INPUTS.ARROW[_.random(3)])
+      sequenceLetter.push(INPUTS.LETTER[_.random(3)])
+    }
+    setArrowSequence(sequenceArrow)
+    setLetterSequence(sequenceLetter)
+  }
+
+  return {
+    generateSequence: () => generateAllSequence(),
+    sequence: {
+      letter: {
+        get: letterSequence,
+        set: (input) => setLetterSequence()
+      },
+      arrow: {
+        get: arrowSequence,
+        set: (input) => setArrowSequence()
+      }
+    }
+  }
+}
+
+export default useGame;
