68b2563623f72c193cca48bd914d8d3d8777e70f
Arix <arixgg@gmail.com>
2021-04-08 02:50:23 +0800

    Shift Game.js styled components to separate style.js file

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,65 +1,19 @@
 import React, { useEffect, useCallback } from 'react'
-import styled, { css } from 'styled-components'
 import useGame from './../../hooks/useGame'
 import Cell from './Cell/Cell'
 import ProgressBar from './ProgressBar/ProgressBar'
+import { Console, StopWatch, ViewSplittor, LeftView, RightView, VerticalBorder } from './style'
 
-const Console = styled.div`
-  display: grid;
-  grid-template-rows: auto 1fr auto auto;
-  width: 100%;
-  justify-items: center;
-`
-const ViewSplittor = styled.div`
-  display: grid;
-  grid-template-columns: 50% 3px 50%;
-  width: 100%;
-  max-width: calc(1000px - calc(2 * var(--default-padding)));
-  align-items: center;
-  overflow: hidden;
-`
-const VerticalBorder = styled.div`
-  background-color: var(--color-text-main);
-  height: 50%;
-  border-radius: 5px;
-  opacity: 0.25;
-`
-const View = styled.div`
-  width: 100%;
-  position: relative;
-  & > *:nth-child(1) {
-    transform: translateX(0) scale(1);
-    filter: brightness(1);
-  }
-`
-const generateCellStyles = (isLeft) => {
-  let styles = ""
-
-  for (let i = 2; i < 30; i++) {
-    styles += `
-      & > *:nth-child(${i}) {
-        transform: translateX(${isLeft ? (-1 * 100 * (i - 1)) : (100 * (i - 1))}px) scale(0.6);
-        filter: brightness(0.8);
-      }
-    `
-  }
-  return css`${styles}`
-}
-
-const LeftView = styled(View)`
-  ${generateCellStyles(true)};
-`
-const RightView = styled(View)`
-  ${generateCellStyles(false)};
-`
-const StopWatch = styled.div`
-  display: grid;
-  place-items: center;
-  font-size: 5rem;
-  color: var(--color-text-main);
-`
 const Game = () => {
-  const { sequence, generateSequence, insertInput, stopWatch, resetGame, progress } = useGame()
+  const { 
+    sequence,
+    generateSequence,
+    insertInput,
+    stopWatch,
+    resetGame,
+    progress 
+  } = useGame()
+
   useEffect(() => {
     generateSequence()
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -71,7 +25,6 @@ const Game = () => {
 
   useEffect(() => {
     window.addEventListener('keydown', handleUserKeyPress);
-
     return () => {
       window.removeEventListener('keydown', handleUserKeyPress);
     };
---
+++ src/components/Game/style.js
@@ -0,0 +1,56 @@
+import styled, { css } from 'styled-components'
+
+export  const Console = styled.div`
+  display: grid;
+  grid-template-rows: auto 1fr auto auto;
+  width: 100%;
+  justify-items: center;
+`
+export const ViewSplittor = styled.div`
+  display: grid;
+  grid-template-columns: 50% 3px 50%;
+  width: 100%;
+  max-width: calc(1000px - calc(2 * var(--default-padding)));
+  align-items: center;
+  overflow: hidden;
+`
+export const VerticalBorder = styled.div`
+  background-color: var(--color-text-main);
+  height: 50%;
+  border-radius: 5px;
+  opacity: 0.25;
+`
+export const View = styled.div`
+  width: 100%;
+  position: relative;
+  & > *:nth-child(1) {
+    transform: translateX(0) scale(1);
+    filter: brightness(1);
+  }
+`
+const generateCellStyles = (isLeft) => {
+  let styles = ""
+
+  for (let i = 2; i < 30; i++) {
+    styles += `
+      & > *:nth-child(${i}) {
+        transform: translateX(${isLeft ? (-1 * 100 * (i - 1)) : (100 * (i - 1))}px) scale(0.6);
+        filter: brightness(0.8);
+      }
+    `
+  }
+  return css`${styles}`
+}
+
+export const LeftView = styled(View)`
+  ${generateCellStyles(true)};
+`
+export const RightView = styled(View)`
+  ${generateCellStyles(false)};
+`
+export const StopWatch = styled.div`
+  display: grid;
+  place-items: center;
+  font-size: 5rem;
+  color: var(--color-text-main);
+`
