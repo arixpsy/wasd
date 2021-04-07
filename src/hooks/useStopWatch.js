d40d50bb1e10fbcc4588b9c52349469a71cb8036
Arix <arixgg@gmail.com>
2021-04-07 23:13:25 +0800

    create custom stopwatch hook

---
+++ src/hooks/useStopWatch.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react'
+
+const useStopWatch = () => {
+  const [isRunning, setIsRunning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
+
+  useEffect(() => {
+      let interval;
+      if (isRunning) {
+        interval = setInterval(
+          () => setElapsedTime(prevElapsedTime => prevElapsedTime + 0.1),
+          100
+        );
+      }
+      return () => clearInterval(interval);
+    },[isRunning]);
+
+    return {
+      elapsedTime: elapsedTime,
+      resetStopWatch: () => setElapsedTime(0),
+      startStopWatch: () => setIsRunning(true),
+      stopStopWatch: () => setIsRunning(false),
+      isRunning: isRunning
+    }
+}
+
+export default useStopWatch
