25e327f5148ad16ecaa041204dedb8b0d09dc24e
arixpsy <arixgg@gmail.com>
2024-03-19 17:03:46 +0800

    lib: install million js

--- million/vite.config.ts
+++ million/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import million from "million/compiler";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [million.vite({ auto: true }), react()],
   server: {
     port: 3000,
   },
