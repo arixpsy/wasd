814ad5acc528266584133b3257a0302d2fa54c51
arixpsy <arixgg@gmail.com>
2024-03-20 23:56:16 +0800

    lib: install million lint

--- million/vite.config.ts
+++ million/vite.config.ts
@@ -2,10 +2,11 @@ import * as path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import million from 'million/compiler'
+import MillionCompiler from '@million/lint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [million.vite({ auto: true }), react()],
+  plugins: [million.vite({ auto: true }), MillionCompiler.vite(), react()],
   server: {
     port: 3000,
   },
