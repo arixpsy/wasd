fc079f6a653dd1356b1d89f4bb6acc613def9d2a
arixpsy <arixgg@gmail.com>
2024-03-20 12:03:42 +0800

    lib: install eslint-plugin-prettier

--- million/vite.config.ts
+++ million/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import million from "million/compiler";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import million from 'million/compiler'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +8,4 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-});
+})
