dd4099750697acfe2ce59cdf28abcd8b748a1178
arixpsy <arixgg@gmail.com>
2024-03-23 01:03:10 +0800

    lib: remove @million/lint
    causing some weird error on hmr

--- million/vite.config.ts
+++ million/vite.config.ts
@@ -2,11 +2,10 @@ import * as path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import million from 'million/compiler'
-import MillionCompiler from '@million/lint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [million.vite({ auto: true }), MillionCompiler.vite(), react()],
+  plugins: [million.vite({ auto: true }), react()],
   server: {
     port: 3000,
   },
