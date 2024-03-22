8a4543b8e21f0cab876b3428d9151dc5bdac515c
arixpsy <arixgg@gmail.com>
2024-03-23 01:01:07 +0800

    wip: make temporary <Content />

---
+++ million/src/components/Content/index.tsx
@@ -0,0 +1,7 @@
+const Content = () => {
+  return <div>
+    Content
+  </div>
+}
+
+export default Content
--- million/src/view/Home.tsx
+++ million/src/view/Home.tsx
@@ -1,12 +1,13 @@
-import Header from '@/components/Header'
+import Content from '@/components/Content'
 import Footer from '@/components/Footer'
+import Header from '@/components/Header'
 
 const Home = () => {
   return (
     <div className='h-screen bg-white p-10 text-gray transition-colors dark:bg-gray dark:text-white'>
       <div className='m-auto grid h-full max-w-[1000px] grid-rows-[auto_1fr_auto]'>
         <Header />
-        <div>Body</div>
+        <Content />
         <Footer />
       </div>
     </div>
