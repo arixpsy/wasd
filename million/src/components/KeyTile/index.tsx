fe161219d9c4e00620ad993bb52a7c5028898e87
arixpsy <arixgg@gmail.com>
2024-04-01 02:38:15 +0800

    tweak: update <KeyTile /> with hidden prop

--- million/src/components/KeyTile/index.tsx
+++ million/src/components/KeyTile/index.tsx
@@ -9,9 +9,10 @@ import { cn } from '@/utils/functions/utils'
 
 type KeyTileProps = {
   keyInput: GameInputs
+  hidden?: boolean
 }
 
-const KeyTile = ({ keyInput }: KeyTileProps) => {
+const KeyTile = ({ keyInput, hidden = false }: KeyTileProps) => {
   const inputCharacter =
     keyInput === GameInputs.UP ? (
       <ImArrowUp />
@@ -28,12 +29,13 @@ const KeyTile = ({ keyInput }: KeyTileProps) => {
   return (
     <div
       className={cn(
-        'grid h-[100px] w-[100px] items-center justify-center rounded-full text-5xl font-bold capitalize text-l-keyText transition-colors dark:text-d-keyText',
+        'grid h-[100px] w-[100px] items-center justify-center rounded-full text-5xl font-bold capitalize text-l-keyText transition-opacity dark:text-d-keyText',
         {
           'bg-key1': GameInputs.UP === keyInput || GameInputs.W === keyInput,
           'bg-key2': GameInputs.LEFT === keyInput || GameInputs.A === keyInput,
           'bg-key3': GameInputs.DOWN === keyInput || GameInputs.S === keyInput,
           'bg-key4': GameInputs.RIGHT === keyInput || GameInputs.D === keyInput,
+          'opacity-0': hidden
         }
       )}
     >
