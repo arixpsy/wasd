1f31f4727c287949878f3e7d707504d1364cf640
arixpsy <arixgg@gmail.com>
2024-03-31 21:50:43 +0800

    tweak: update <IconButton /> to allow className overwrite

--- million/src/components/IconButton/index.tsx
+++ million/src/components/IconButton/index.tsx
@@ -1,13 +1,17 @@
+import { cn } from '@/utils/functions/utils'
 import { PropsWithChildren } from 'react'
 
 type IconButtonProps = React.ButtonHTMLAttributes<
   HTMLButtonElement & PropsWithChildren
 >
 
-const IconButton = ({ children, ...rest }: IconButtonProps) => {
+const IconButton = ({ children, className, ...rest }: IconButtonProps) => {
   return (
     <button
-      className='rounded-lg p-1 text-3xl outline-gray-200 transition-colors hover:bg-gray-200 dark:text-d-text dark:outline-white dark:hover:bg-transparent dark:hover:text-white'
+      className={cn(
+        'rounded-lg p-1 text-3xl outline-gray-200 transition-colors hover:bg-gray-200 dark:text-d-text dark:outline-white dark:hover:bg-transparent dark:hover:text-white',
+        className
+      )}
       {...rest}
     >
       {children}
