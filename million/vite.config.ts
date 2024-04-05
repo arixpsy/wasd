5278747a4e835aa61b37f98ba8516c1878c0e91b
arixpsy <arixgg@gmail.com>
2024-04-05 17:10:26 +0800

    lib: install vite-plugin-checker

--- million/vite.config.ts
+++ million/vite.config.ts
@@ -1,11 +1,21 @@
 import * as path from 'path'
 import { defineConfig } from 'vite'
+import checker from 'vite-plugin-checker'
 import react from '@vitejs/plugin-react-swc'
 import million from 'million/compiler'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [million.vite({ auto: true }), react()],
+  plugins: [
+    million.vite({ auto: true }),
+    react(),
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+      },
+    }),
+  ],
   server: {
     port: 3000,
   },
