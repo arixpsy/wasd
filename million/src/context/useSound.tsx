3e098a6fd16fa2d981a8cb4ea77a4f419e2021b2
arixpsy <arixgg@gmail.com>
2024-04-09 03:18:47 +0800

    wip: update sound provider with update functions

--- million/src/context/useSound.tsx
+++ million/src/context/useSound.tsx
@@ -6,11 +6,17 @@ import { Sound } from '@/types'
 type SoundContext = {
   playKeySuccess: () => void
   playKeyError: () => void
+  changeKeySuccess: (sound: Sound, volume: number) => void
+  changeKeyError: (sound: Sound, volume: number) => void
+  updateVolume: (volume: number) => void
 }
 
 export const SoundContext = createContext<SoundContext>({
   playKeySuccess: () => {},
   playKeyError: () => {},
+  changeKeySuccess: () => {},
+  changeKeyError: () => {},
+  updateVolume: () => {},
 })
 
 const SoundProvider = ({ children }: PropsWithChildren) => {
@@ -30,17 +36,25 @@ const SoundProvider = ({ children }: PropsWithChildren) => {
     sound: Sound,
     volume: number
   ) => {
-    if (type === 'success') {
-      keySuccessSoundRef.current = new Howl({
-        src: [sound],
-        volume,
-      })
-    } else {
-      keyErrorSoundRef.current = new Howl({
+    let newSound = null
+
+    if (sound !== '') {
+      newSound = new Howl({
         src: [sound],
         volume,
       })
     }
+
+    if (type === 'success') {
+      keySuccessSoundRef.current = newSound
+    } else {
+      keyErrorSoundRef.current = newSound
+    }
+  }
+
+  const updateVolume = (volume: number) => {
+    if (keySuccessSoundRef.current) keySuccessSoundRef.current.volume(volume)
+    if (keyErrorSoundRef.current) keyErrorSoundRef.current.volume(volume)
   }
 
   useEffect(() => {
@@ -60,6 +74,11 @@ const SoundProvider = ({ children }: PropsWithChildren) => {
       value={{
         playKeySuccess: () => handlePlaySound('success'),
         playKeyError: () => handlePlaySound('error'),
+        changeKeySuccess: (sound: Sound, volume: number) =>
+          initilizeSound('success', sound, volume),
+        changeKeyError: (sound: Sound, volume: number) =>
+          initilizeSound('error', sound, volume),
+        updateVolume,
       }}
     >
       {children}
